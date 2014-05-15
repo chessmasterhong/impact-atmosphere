16d2a88ff38d19ec06e7309072ab726c14adf2ea
Kevin Chan <chessmasterhong@yahoo.com>
2014-05-15 07:41:20 -0400

    Added bugs and known issues section to readme. Updated todo in readme.

--- demo/lib/game/main.js
+++ demo/lib/game/main.js
@@ -12,6 +12,7 @@ ig.module(
         init: function() {
             // Initialize Day/Night Cycle Plugin
             // Start from current date and time, updating every 0.5 seconds, running at 600x real time
+            // (0.5 real_sec/update) * (600 plugin_secs/real_sec) = (300 plugin_secs/update) * (1/60 plugin_min/plugin_sec) = 5 plugin_mins/update
             this.daynight = new ig.DayNight(new Date(), 0.5, 600);
 
             ig.input.bind(ig.KEY.MOUSE1, 'click');
