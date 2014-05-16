4cd9574451c247c4994bbcbb7e787bbe358b6b8b
Kevin Chan <chessmasterhong@yahoo.com>
2014-05-16 17:24:01 -0400

    Initial commit for weather branch. Added barebones plugin.

--- demo/lib/game/main.js
+++ demo/lib/game/main.js
@@ -5,7 +5,8 @@ ig.module(
     'impact.game',
     'game.levels.demo',
     'game.levels.demo2',
-    'plugins.day-night'
+    'plugins.day-night',
+    'plugins.weather'
 )
 .defines(function() {
     var MainGame = ig.Game.extend({
@@ -14,6 +15,7 @@ ig.module(
             // Start from current date and time, updating every 0.5 seconds, running at 600x real time
             // (0.5 real_sec/update) * (600 plugin_secs/real_sec) = (300 plugin_secs/update) * (1/60 plugin_min/plugin_sec) = 5 plugin_mins/update
             this.daynight = new ig.DayNight(new Date(), 0.5, 600);
+            this.weather = new ig.Weather();
 
             ig.input.bind(ig.KEY.MOUSE1, 'click');
 
---
+++ demo/lib/plugins/weather.js
@@ -0,0 +1,45 @@
+/**
+ *  weather.js
+ *  -----
+ *  Impact Weather Add-on for the Impact Day/Night Cycle Plugin
+ *  https://github.com/chessmasterhong/impact-day-night
+ *
+ *  Kevin Chan (chessmasterhong)
+ *
+ *  An add-on for the Impact Day/Night Cycle Plugin for the Impact game engine
+ *  that simulates weather conditions.
+ */
+
+
+ig.module(
+    'plugins.weather'
+)
+.requires(
+    'impact.game'
+)
+.defines(function() {
+    "use strict";
+
+    ig.Weather = ig.Game.extend({
+        //---------------------------------------------------------------------
+        // Init
+        init: function() {
+            // TODO
+        }, // End init
+        //---------------------------------------------------------------------
+
+        //---------------------------------------------------------------------
+        // Update
+        update: function() {
+            // TODO
+        }, // End update
+        //---------------------------------------------------------------------
+
+        //---------------------------------------------------------------------
+        // Draw
+        draw: function() {
+            // TODO
+        }, // End draw
+        //---------------------------------------------------------------------
+    });
+});
