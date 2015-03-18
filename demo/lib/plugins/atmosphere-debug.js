9f4e75a721dc6edf620a980803f19dd6e2e68695
Kevin Chan <chessmasterhong@yahoo.com>
2015-03-17 21:41:30 -0400

    Added .jshintrc. Fixed JSHint camelcase warnings.

--- demo/lib/plugins/atmosphere-debug.js
+++ demo/lib/plugins/atmosphere-debug.js
@@ -15,7 +15,6 @@ ig.module(
     'use strict';
 
     ig.Atmosphere.inject({
-
         debug: true,
 
         draw: function() {
@@ -40,17 +39,17 @@ ig.module(
                 ig.system.context.fillText('========== Impact Atmospheric System Plugin ==========', x += 5, y += 5);
 
                 ig.system.context.fillText('Timescale: ' + this.timescale + 'x real time', x, y += 15);
-                ig.system.context.fillText('Update rate: ' + this.update_rate + (this.update_rate <= 1 ? ' second' : ' seconds'), x, y += 10);
+                ig.system.context.fillText('Update rate: ' + this.updateRate + (this.updateRate <= 1 ? ' second' : ' seconds'), x, y += 10);
 
-                ig.system.context.fillText('Geographical coordinates: (Lat: ' + this.geo_coords.latitude + ', Lng: ' + this.geo_coords.longitude + ')', x, y += 15);
+                ig.system.context.fillText('Geographical coordinates: (Lat: ' + this.geoCoords.latitude + ', Lng: ' + this.geoCoords.longitude + ')', x, y += 15);
 
                 ig.system.context.fillStyle = '#ffff00';
                 ig.system.context.fillText('Current: ' + this.convertJulianToGregorian(this.julianDate).toString() + ' | ' + this.julianDate.toFixed(8) + ' JD', x, y += 15);
                 ig.system.context.fillText('Sun state: The sun ' + (
-                    this.sun_state === 0 ? 'is rising' :
-                    this.sun_state === 1 ? 'has risen' :
-                    this.sun_state === 2 ? 'is setting' :
-                    this.sun_state === 3 ? 'has set' :
+                    this.sunState === 0 ? 'is rising' :
+                    this.sunState === 1 ? 'has risen' :
+                    this.sunState === 2 ? 'is setting' :
+                    this.sunState === 3 ? 'has set' :
                     '<invalid sun state>'
                 ), x, y += 15);
 
@@ -60,40 +59,40 @@ ig.module(
                 ig.system.context.fillText('Sunrise: ' + this.convertJulianToGregorian(this.solar.sunrise.date).toString() + ' | ' + this.solar.sunrise.date.toFixed(8) + ' JD', x, y += 15);
                 ig.system.context.fillText('Sunset : ' + this.convertJulianToGregorian(this.solar.sunset.date).toString() + ' | ' + this.solar.sunset.date.toFixed(8) + ' JD', x, y += 10);
 
-                ig.system.context.fillText('Next sunriset update: ' + this.convertJulianToGregorian(this.solar.next_update).toString(), x, y += 15);
+                ig.system.context.fillText('Next sunriset update: ' + this.convertJulianToGregorian(this.solar.nextUpdate).toString(), x, y += 15);
 
                 ig.system.context.fillStyle = '#ffff00';
                 ig.system.context.fillText('Season state: ' + (
-                    this.season_state === 0 ? 'Spring/Vernal' :
-                    this.season_state === 1 ? 'Summer/Estival' :
-                    this.season_state === 2 ? 'Autumn/Autumnal' :
-                    this.season_state === 3 ? 'Winter/Hibernal' :
+                    this.seasonState === 0 ? 'Spring/Vernal' :
+                    this.seasonState === 1 ? 'Summer/Estival' :
+                    this.seasonState === 2 ? 'Autumn/Autumnal' :
+                    this.seasonState === 3 ? 'Winter/Hibernal' :
                     '<invalid season state>'
                 ), x, y += 15);
 
                 ig.system.context.fillStyle = '#ffffff';
-                ig.system.context.fillText('Spring : ' + this.convertJulianToGregorian(this.season.vernal_equinox).toString() + ' | ' + this.season.vernal_equinox.toFixed(8) + ' JD', x, y += 15);
-                ig.system.context.fillText('Summer : ' + this.convertJulianToGregorian(this.season.estival_solstice).toString() + ' | ' + this.season.estival_solstice.toFixed(8) + ' JD', x, y += 10);
-                ig.system.context.fillText('Autumn : ' + this.convertJulianToGregorian(this.season.autumnal_equinox).toString() + ' | ' + this.season.autumnal_equinox.toFixed(8) + ' JD', x, y += 10);
-                ig.system.context.fillText('Winter : ' + this.convertJulianToGregorian(this.season.hibernal_solstice).toString() + ' | ' + this.season.hibernal_solstice.toFixed(8) + ' JD', x, y += 10);
+                ig.system.context.fillText('Spring : ' + this.convertJulianToGregorian(this.season.vernalEquinox).toString() + ' | ' + this.season.vernalEquinox.toFixed(8) + ' JD', x, y += 15);
+                ig.system.context.fillText('Summer : ' + this.convertJulianToGregorian(this.season.estivalSolstice).toString() + ' | ' + this.season.estivalSolstice.toFixed(8) + ' JD', x, y += 10);
+                ig.system.context.fillText('Autumn : ' + this.convertJulianToGregorian(this.season.autumnalEquinox).toString() + ' | ' + this.season.autumnalEquinox.toFixed(8) + ' JD', x, y += 10);
+                ig.system.context.fillText('Winter : ' + this.convertJulianToGregorian(this.season.hibernalSolstice).toString() + ' | ' + this.season.hibernalSolstice.toFixed(8) + ' JD', x, y += 10);
 
                 var wc = 'Clear';
-                if(this.weather_condition.rain || this.weather_condition.snow || this.weather_condition.fog) {
+                if(this.weatherCondition.rain || this.weatherCondition.snow || this.weatherCondition.fog) {
                     wc = '';
-                    if(this.weather_condition.rain)      { wc += 'Rain ';      }
-                    if(this.weather_condition.snow)      { wc += 'Snow ';      }
-                    if(this.weather_condition.lightning) { wc += 'Lightning '; }
-                    if(this.weather_condition.fog)       { wc += 'Fog ';       }
+                    if(this.weatherCondition.rain)      { wc += 'Rain ';      }
+                    if(this.weatherCondition.snow)      { wc += 'Snow ';      }
+                    if(this.weatherCondition.lightning) { wc += 'Lightning '; }
+                    if(this.weatherCondition.fog)       { wc += 'Fog ';       }
                 }
 
                 ig.system.context.fillStyle = '#ffff00';
                 ig.system.context.fillText('Weather condition: ' + wc, x, y += 15);
 
                 ig.system.context.fillStyle = '#ffffff';
-                ig.system.context.fillText('Maximum Particle Count: ' + this.particles_max, x, y += 15);
-                ig.system.context.fillText('Current Particle Count: ' + this.particles_curr, x, y += 10);
+                ig.system.context.fillText('Maximum Particle Count: ' + this.particlesMax, x, y += 15);
+                ig.system.context.fillText('Current Particle Count: ' + this.particlesCurr, x, y += 10);
 
-                //if(this.weather_condition.fog) {
+                //if(this.weatherCondition.fog) {
                 //    ig.system.context.fillText('Fog block size: ' + size + 'px * ' + size + 'px', x, y += 15);
                 //    ig.system.context.fillText('Fog block iterations: ' + Math.ceil(ig.system.width / size) + ' * ' + Math.ceil(ig.system.height / size) + ' = ' + Math.ceil((ig.system.width * ig.system.height) / (size * size)), x, y += 10);
                 //}
